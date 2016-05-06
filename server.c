64fadf7032141ed6f901fbfb116932378b9d3d0c
Arcadian Computers, LLC <support@arcadiancomputers.com>
2016-05-06 17:44:47 -0400

    Need a lookup function or we just increment icp[] until it's full.

--- server.c
+++ server.c
@@ -10,7 +10,10 @@ int main(int argc, char **argv)
 {
 	int i=0;				/* loop counter */
 	int re=0;				/* return holder */
+	int ra = 1;				/* reuse address */
 	int ic[256];				/* array of incoming connections */
+	int nc[256][1];				/* index into cc for current string */
+	char cc[256][256];			/* array of incoming strings */
 	int icp=0;				/* current fd of next incoming connection */
 	int prt=0;				/* port return */
 	struct sockaddr_in sin; 		/* socket structure */
@@ -22,6 +25,12 @@ int main(int argc, char **argv)
 	memset(&sin,0,sizeof(sin));		/* clear it out */
 	memset(&ic,0,sizeof(ic)); 		/* clear it out */
 
+	for (i = 0; i < 256; i++) 		/* clear it out */
+	{
+		memset(cc[i],0,sizeof(cc));
+		memset(nc[i],0,sizeof(nc));
+	}
+
 	prt = socket(AF_INET,SOCK_STREAM,0);
 
 	if (prt == -1)
@@ -31,6 +40,12 @@ int main(int argc, char **argv)
 	}
 
 	fcntl(prt,F_SETFL,O_NONBLOCK);		/* non-block i/o */
+
+	if (setsockopt(prt,SOL_SOCKET,SO_REUSEADDR,&ra,sizeof(ra)) == -1)
+	{
+		perror("setsockopt(): ");	/* set the listening port reuseable to prevent TIME_WAIT blocking */
+		return -1;
+	}
 
 	sin.sin_family = AF_INET;		/* address family */
 	sin.sin_port = htons(SRV_PORT); 	/* tcp port to listen on */
@@ -80,6 +95,8 @@ int main(int argc, char **argv)
 		
 			if (rc == 1)
 			{
+				cc[i][nc[i][]] = c;
+
 				if (c[0] >= 32) printf("Got a byte: \"%s\"\t[%i]\n",c,c[0]);
 				else if (c[0] < 32 || c[0] > 126) printf("Got a byte: \"\"\t[%i]\n",c[0]);
 			}
